5879a0120d064d13a7d6d1a16953b88535187508
shalu kumari <shalukumari@shalus-MacBook-Air.local>
2025-11-09 11:52:56 +0530

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.get('/twitter', (req, res) => {
+  res.send('shaludotcom')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Please login at shalu dot com</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+  res.send("<h2>learn server</h2>")
+})
+
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
