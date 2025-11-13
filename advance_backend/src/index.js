a6d59dd19c7ec1ec82396bfa1369a1d18370b25a
shalu kumari <shalukumari@shalus-MacBook-Air.local>
2025-11-13 19:57:00 +0530

    connect to database

---
+++ advance_backend/src/index.js
@@ -0,0 +1,43 @@
+// require('dotenv').config()
+// or
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import {DB_NAME} from './constants.js';
+import express from "express";
+import connectDB from "./db/index.js";
+const app = express();
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+// here we use effi ;()()
+// ; semicolon isliye use karte hai kyu ki agar phele wale line me semicolon na laga ho to koi error na aaye
+/*( async() => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) // database connect hogaya
+        app.on("error", (error) => {
+            console.log("ERROR", error)
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("ERROR", error)
+        throw error;
+    }
+})()
+    */
+   
+//ye tha first approach db ko connect karne ka 
