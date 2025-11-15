a1f1150cf34db11b3ab630abe906d4813faa1fe4
shalu kumari <shalukumari@shalus-MacBook-Air.local>
2025-11-15 12:13:16 +0530

    Custom api response and error handling

---
+++ advance_backend/src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// now we see how to write same code in try catch way
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {} means (func) => {() => {}}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (requestHandler) => async(req, res, next) => {
+//     try {
+//         await requestHandler(req, res, next)
+//     } catch (error) {
+//         // err.code means agar user error pass kar raha hai  500 means agar user error pass na kar raha ho tab
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
